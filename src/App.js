9258bcacb961d75e243536bc76b19d7606051d68
lauragomez04 <lauragomezb0904@gmail.com>
2022-03-20 17:55:48 +0000

    Project Setup + Bootstrap install

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-center mt-5">Dictionary</h1>
+    </div>
+  );
+}
+
+export default App;
