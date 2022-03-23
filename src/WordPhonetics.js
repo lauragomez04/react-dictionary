be5ad719c3cd0800968b52456970b3f3084b1c85
lauragomez04 <lauragomezb0904@gmail.com>
2022-03-23 16:38:30 +0000

    Update WordPhonetics.js

--- src/WordPhonetics.js
+++ src/WordPhonetics.js
@@ -10,7 +10,7 @@ export default function WordPhonetics(props) {
   });
 
   const handleClick = () => {
-    setPlaybackRate(playbackRate + 0.1);
+    setPlaybackRate(playbackRate);
     play();
   };
 
